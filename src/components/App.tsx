80b777a117f3ca5e14286ae5002af4b1fa93a6cf
Nathan Brewer <nate@devslopes.com>
2018-04-11 17:55:55 -0700

    pushing project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,33 @@
 import * as React from "react";
 import "./../assets/scss/App.scss";
 
-const reactLogo = require("./../assets/img/react_logo.svg");
-
-export interface AppProps {
+type State = {
+    pwdInput: string
 }
 
-export default class App extends React.Component<AppProps, undefined> {
+export default class App extends React.Component<{}, State> {
+    constructor(props: any) {
+        super(props);
+        this.state = {pwdInput: ''}
+    };
+
+    displayAccount = () => {
+
+    };
     render() {
         return (
             <div className="app">
-                <h1>Hello World!</h1>
-                <p>Foo to the barz</p>
-                <img src={reactLogo} height="480"/>
+                <h1>Create a Simple Wallet!</h1>
+                <div className="pwdInput">
+                    <div>
+                        <div>Password:</div>
+                    </div>
+                    <div>
+                        <input onChange={(event) => this.setState({pwdInput: event.target.value})} placeholder={"This will be used to access your account"} type="search"/>
+                    </div>
+                </div>
+                <button onClick={() => console.log(this.state.pwdInput)} type="button" className={"createWallet"}>CREATE</button>
+                {/*{this.displayAccount()}*/}
             </div>
         );
     }
